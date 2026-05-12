aa5c0d2d5df213c2b737e3cfa9504f394277edae
Allienino <ajayisultan39@gmail.com>
2026-05-12 12:54:10 +0100

    feat: Introduction

---
+++ intro.js
@@ -0,0 +1,5 @@
+let name = "Sultan Ajayi"
+let age = "rather not say"
+let bestProgLang = "Python"
+
+console.log (`My name is ${name} and as for my age,I would ${age} and my best programming language is ${bestProgLang}.`)
